1f248481b45bf407146511938045f81bb9d55655
SVillaluz <sebastianvillaluz99@gmail.com>
2025-03-04 14:22:41 -0600

    Se creo una carpeta llamada templates donde dentro de ella se creo un documento llamado index.html el cual se ocupara para la creación del frontend de la pagina mismo al cual se le pasaran valores de un diccionario creado en el documento app.py

--- app/app.py
+++ app/app.py
@@ -1,6 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/')
+def index():
+   # Creación de un diccionario
+   # Este diccionario puede mandar sus valores al index.html
+   cursos = ['PHP', 'Python', 'Java', 'Kotlin', 'Dart', 'JavaScript']
+   data={
+       'titulo':'Index123',
+       'bienvenida':'Saludos!',
+       'cursos':cursos,
+       'numero_cursos': len(cursos)
+   }
+   return render_template('index.html', data=data)
 
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
