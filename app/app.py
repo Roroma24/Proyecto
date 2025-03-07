a3b9c4aad7649102946489486c409bbc3f90f895
Roroma24 <a010164537@my.uvm.edu.mx>
2025-03-06 20:35:53 -0600

    restaurar app.py

--- app/app.py
+++ app/app.py
@@ -4,19 +4,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-   cursos = ['PHP', 'Python', 'Java', 'Kotlin', 'Dart', 'JavaScript']
-   data = {
-       'titulo': 'Index123',
-       'bienvenida': '¡Saludos!',
-       'cursos': cursos,
-       'numero_cursos': len(cursos)
-   }
-   return render_template('index.html', data=data)
+    # Creación de un diccionario
+    # Este diccionario puede mandar sus valores al index.html
+    cursos = ['PHP', 'Python', 'Java', 'Kotlin', 'Dart', 'JavaScript']
+    data = {
+        'titulo': 'Index123',
+        'bienvenida': 'Saludos!',
+        'cursos': cursos,
+        'numero_cursos': len(cursos)
+    }
+    return render_template('index.html', data=data)
 
-@app.route('/inicio')
-def inicio():
-   return render_template('inicio.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
-
