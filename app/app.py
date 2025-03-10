baca73e1513421fb999b80f56a84cbdae767d465
SVillaluz <sebastianvillaluz99@gmail.com>
2025-03-10 04:06:48 -0600

    Elimine el layout puesto que en este punto vi que era mejor trabajar las paginas web hechas en html por separado asi mismo continue con la edición de la pagina del menu principal y se le hizo una pequeña modificación al 2.html donde elimine el boton que decia registro como doctor ya que el proposito de ese boton es el de mandar al usuario a la pagina en la que ya se encontraba.

--- app/app.py
+++ app/app.py
@@ -4,16 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Creación de un diccionario 
-    # Este diccionario puede mandar sus valores al index.html
-    cursos = ['PHP', 'Python', 'Java', 'Kotlin', 'Dart', 'JavaScript']
-    data={ 
-        'titulo':'Index123', 
-        'bienvenida':'Saludos!', 
-        'cursos':cursos, 
-        'numero_cursos': len(cursos) 
-    }
-    return render_template('index.html', data=data)
+    return render_template('index.html')
 
 if __name__ == '__main__': 
     app.run(debug=True, port=5000)
