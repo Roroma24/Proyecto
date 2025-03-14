02781783a27c267bbc56bfbbd19eeffc31114222
SVillaluz <sebastianvillaluz99@gmail.com>
2025-03-14 10:18:34 -0600

    Termine la pagina del menu principal asi como la de registro de usuario a esta ultima solo falta acomodar bien junto con todo esto empeze a hacer los enlaces entre las paginas

--- app/app.py
+++ app/app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/registro')
+def registro():
+    return render_template('registrouser.html')
+
+@app.route('/exit')
+def exit():
+    return render_template('index.html')
+
+@app.route('/cancel')
+def cancel():
+    return render_template('cancelacion.html')
+
 if __name__ == '__main__': 
     app.run(debug=True, port=5000)
