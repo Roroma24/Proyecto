0cc6153308fb81b264f728522aa7beaaf7f5dd3d
SVillaluz <sebastianvillaluz99@gmail.com>
2025-03-22 14:25:28 -0600

    Se cargaron todas las modificaciones hasta el punto actual

--- app/app.py
+++ app/app.py
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-from flask import Flask, render_template, request, redirect, url_for
-=======
 """
 Autores: 
 Axel Castañeda Sánchez 
@@ -9,13 +6,12 @@ Alyn Layla Nila Vélez
 Luis Roberto Rodríguez Marroquin 
 Sebastián Villaluz Martínez
 Daniela Zuno Aguilar
->>>>>>> 643bdc13029ed472aaeefdc8c95fab140dd41c70
 
 Descripción:
 Este archivo contiene el código para la creación de la aplicación web con Flask.
 """
 
-from flask import Flask, render_template  # Importamos Flask y render_template para manejar nuestra aplicación web.
+from flask import Flask, render_template, request, redirect, url_for  # Importamos Flask y render_template para manejar nuestra aplicación web.
 
 app = Flask(__name__)  # Creamos una instancia de Flask.
 
@@ -29,7 +25,6 @@ def index():
 def login():
     return render_template('login.html')  # Renderiza la página HTML llamada 'login.html'.
 
-<<<<<<< HEAD
 @app.route('/registro')
 def registro():
     return render_template('registrouser.html')
@@ -38,14 +33,10 @@ def registro():
 def newdoctor():
     return render_template('registrodoc.html')
 
-=======
-# Ruta para la página de reservaciones (reservation.html).
->>>>>>> 643bdc13029ed472aaeefdc8c95fab140dd41c70
 @app.route('/reserva')
 def reserva():
     return render_template('reservation.html')  # Renderiza la página HTML llamada 'reservation.html'.
 
-<<<<<<< HEAD
 @app.route('/pago')
 def pago():
     return render_template('metodopago.html')
@@ -79,28 +70,11 @@ def modification():
 def cancel():
     folio = request.args.get('folio', '')
     return render_template('cancelacion.html', folio=folio)
-=======
-# Ruta para la página de registro de usuarios (registrouser.html).
-@app.route('/registro')
-def registro():
-    return render_template('registrouser.html')  # Renderiza la página HTML llamada 'registrouser.html'.
->>>>>>> 643bdc13029ed472aaeefdc8c95fab140dd41c70
 
 # Ruta para salir (redirige a la página principal).
 @app.route('/exit')
 def exit():
     return render_template('index.html')  # Renderiza nuevamente la página 'index.html'.
 
-<<<<<<< HEAD
 if __name__ == '__main__': 
     app.run(debug=True)
-=======
-# Ruta para cancelar (cancelacion.html).
-@app.route('/cancel')
-def cancel():
-    return render_template('cancelacion.html')  # Renderiza la página HTML llamada 'cancelacion.html'.
-
-# Configuración para ejecutar la aplicación en modo depuración en el puerto 5000.
-if __name__ == '__main__': 
-    app.run(debug=True, port=5000)  # Ejecuta la app con depuración activa y en el puerto 5000.
->>>>>>> 643bdc13029ed472aaeefdc8c95fab140dd41c70
